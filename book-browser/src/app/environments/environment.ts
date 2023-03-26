10d396610b26c91073e2f0f1895d37f9db9db9e5
Sebastian Jakobowski <SebastianJakobowski@outlook.de>
2023-03-26 16:25:21 +0200

    creates api environment

---
+++ book-browser/src/app/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    env: "Test",
+    api: {
+        openlibrary: "https://openlibrary.org",
+        coversopenlibrary: "http://covers.openlibrary.org"
+    }
+}
--- book-browser/src/app/models/book-model.ts
+++ book-browser/src/app/models/book-model.ts
@@ -1,2 +1,26 @@
 export interface BookModel {
+    isbn: string;
+    title: string;
+    first_publish_year: number;
+    cover_i: number;
+    author_name: string;
+}
+
+export class Book implements BookModel {
+    isbn!: string;
+    title!: string;
+    first_publish_year!: number;
+    cover_i!: number;
+    author_name!: string;
+
+    deserialize(input: any): this {
+        Object.assign(this, input);
+        return this;
+    }
+
+    static deserialize(books: BookModel[]): Book[] {
+        let result: Book[] = [];
+        books.forEach((value, index) => (result[index] = new Book().deserialize(value)));
+        return result;
+    }
 }
