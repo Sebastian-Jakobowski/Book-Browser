5f3f24a2e2e7c7e7ab10c1c50b45486f1392e674
Sebastian Jakobowski <SebastianJakobowski@outlook.de>
2023-03-26 16:47:26 +0200

    creates first service function

--- book-browser/src/app/services/book.service.ts
+++ book-browser/src/app/services/book.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { BookModel } from '../models/book-model';
+import axios from "axios";
+import { environment } from 'src/app/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +9,18 @@ import { Injectable } from '@angular/core';
 export class BookService {
 
   constructor() { }
+
+  getBooks(title: string): Promise<BookModel[]>{
+    return new Promise<BookModel[]>(((resolve, reject) => {
+      axios.get(`${environment.api.openlibrary}/search.json`, {
+        params: {
+          title: title,
+        },
+      }).then((axiosResponse) => {
+        resolve(axiosResponse.data.docs as BookModel[])
+      }).catch((reason: any) => {
+        reject(reason);
+      })
+    }))
+  }
 }
